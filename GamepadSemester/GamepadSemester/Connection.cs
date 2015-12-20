46135bc136a1aaea546f24ce659a3e04dec4b0b8
gf2595 <Ehre47593Treue>
2015-12-20 19:38:43 +0300

    Semester Task Initial Commit

---
+++ GamepadSemester/GamepadSemester/Connection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace GamepadClientNamespace
+{
+    class GamepadClient
+    {
+        static private StreamSocket clientSocket;
+        static private HostName serverHost;
+        static private string serverHostnameString = "";
+        static private string serverPort = "";
+        static private bool connected = false;
+        static private bool closing = false;
+
+        static public async void Connect(string address, string port)
+        {
+            clientSocket = new StreamSocket();
+
+            try
+            {
+                serverHost = new HostName(address);
+                serverPort = port;
+                serverHostnameString = address;
+
+                await clientSocket.ConnectAsync(serverHost, serverPort);
+                connected = true;
+
+            }
+            catch (Exception)
+            {                
+                clientSocket.Dispose();
+                clientSocket = null;
+                closing = true;
+                
+                //throw new ConnectionErrorException();
+            }
+        }
+
+        static public async void Send(int number)
+        {
+            if (connected)
+            {
+                try
+                {
+                    DataWriter writer = new DataWriter(clientSocket.OutputStream);
+                    writer.WriteString(number.ToString());
+
+                    await writer.StoreAsync();
+
+                    writer.DetachStream();
+                    writer.Dispose();
+                }
+                catch (Exception)
+                {
+                    throw new SendingErrorException();
+                }
+            }
+        }
+
+        static public void getCurrentIPAdressAndPort(out string IPAddress, out string port)
+        {
+            IPAddress = serverHostnameString;
+            port = serverPort;
+        }
+    }
+}
---
+++ GamepadSemester/GamepadSemester/GamepadPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GamepadClientNamespace;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace GamepadSemester
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class GamepadPage : Page
+    {
+        public GamepadPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Вызывается перед отображением этой страницы во фрейме.
+        /// </summary>
+        /// <param name="e">Данные события, описывающие, каким образом была достигнута эта страница.
+        /// Этот параметр обычно используется для настройки страницы.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+            int buttonsAmount = (int)e.Parameter;
+            ChangeButtonsAmount(buttonsAmount);
+            }
+            catch (InvalidCastException)
+            { }
+        }
+
+        private void TurnPage(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(SettingsPage), ButtonsPanel.Children.Count);
+        }
+
+        private void ButtonPressed(object sender, RoutedEventArgs e)
+        {
+            Button pressedButton = (Button)sender;
+
+            try
+            {
+                int buttonNumber = 0;
+                int.TryParse(pressedButton.Content.ToString(), out buttonNumber);
+
+                if (pressedButton.Name == "AdditionalButton")
+                {
+                    buttonNumber += 8;
+                }
+
+                GamepadClient.Send(buttonNumber);
+            }
+            catch (SendingErrorException)
+            {
+                ErrorBlock.Text = "Sending error. Check receiver settings";
+            }
+
+        }
+
+        public void ChangeButtonsAmount(int amount)
+        {
+            ButtonsPanel.Children.Clear();
+
+            for (int i = 0; i < amount; i++)
+            {
+                Button temp = new Button();
+                temp.Name = "AdditionalButton";
+                temp.Template = ButtonTemplate;
+                temp.Content = i + 1;
+                temp.Click += ButtonPressed;
+                ButtonsPanel.Children.Add(temp);
+                ButtonsPanel.Width += temp.Width;
+            }
+        }
+    }
+}
